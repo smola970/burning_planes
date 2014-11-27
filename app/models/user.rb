0a49f44f204cb56a215bb70ca1d71fe7f65ab8d2
Robert Will <mail.rwill@gmail.com>
2014-11-27 14:04:03 +1100

    users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+has_and_belongs_to_many :reservations
+
+# has_secure_password
+
+  validates :name, :uniqueness => true
+  # validates :password_confirmation, :presence => true
+
 end
