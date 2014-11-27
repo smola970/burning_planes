b42b27590baeae90c42a0fa7cd36021f6aa8c284
Lauren Smolenski <lauren@smolenski.net.nz>
2014-11-27 15:04:19 +1100

    pages controller and home page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  root 'pages#index'
 
-  root '/login' => 'session#new'
+  get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy', :as => 'logout'
 
