04c46a01d76f7915aa9a40766abb94aae174ea4a
Lauren Smolenski <lauren@smolenski.net.nz>
2014-11-27 14:52:16 +1100

    conflicts in routes edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
+
   root '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy', :as => 'logout'
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   get '/sign_up' => 'users#new'
   post '/sign_up' => 'users#create'
 
-=======
->>>>>>> 3c9d1c26f901d40b5bb1b79b81686d4749710c5d
   resources :flights
   resources :airplanes
 end
