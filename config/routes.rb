fa668dbfdb9d015e35b55479fe3bd96f02bfd82f
Jam Creencia <jrcreencia@gmail.com>
2014-11-27 15:04:51 +1100

    merging

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  root '/login' => 'session#new'
+  get '/login' => 'session#new'
   post '/login' => 'session#create'
   delete '/login' => 'session#destroy', :as => 'logout'
 
