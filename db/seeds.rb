6bd59811f56743f4e3b9320adadca9ac14b4cca8
Robert Will <mail.rwill@gmail.com>
2014-11-28 09:04:39 +1100

    fixed login

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin = User.create(username: "admin", password: "password", password_confirmation: "password", admin: "true")
+User.destroy_all
 
-user = User.create(username: "customer", password: "password", password_confirmation: "password", admin: "false")
+admin = User.create(username: "admin", password: "password", password_confirmation: "password", admin: true)
+
+user = User.create(username: "customer", password: "password", password_confirmation: "password", admin: false)
