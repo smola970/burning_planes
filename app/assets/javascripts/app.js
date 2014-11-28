5fe644eecf18a0f3eca4aea8053497cb2f96888c
Jam Creencia <jrcreencia@gmail.com>
2014-11-28 15:50:32 +1100

    seats working

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,13 +1,14 @@
 //new collection
-App.seats = new App.Collections.Seats();
+// App.seats = new App.Collections.Seats();
 
-//fetch seats from back end
-App.seats.fetch().done(function() {
+// //fetch seats from back end
+// // App.seats.fetch().done(function() {
 
-//when done fetching from back end
+// //when done fetching from back end
 
-App.seats.each(function(seat, index) {
-  var view = new App.Views.SeatView({ model: seat});
-    $('.seats').append(view.render().el);
-  });
-});
+// App.seats.each(function(seat, index) {
+//   var view = new App.Views.SeatView({ model: seat});
+//     $('.seats').append(view.render().el);
+//   });
+// });
+
--- app/assets/javascripts/views/seat_view.js
+++ app/assets/javascripts/views/seat_view.js
@@ -5,25 +5,25 @@ App.Views.SeatView = Backbone.View.extend({
   tagName: 'div',
 
   events: {
-    'click': selectSeat
-  },
-
-  initialize: function() {
-    this.listenTo(this.model,'change', this.render);
+    'click': 'selectSeat'
   },
 
   render: function() {
-    var template = $('#seatView').html();
-    var seatHTML = Handlebars.compile(template);
 
-    this.$el.html(seatHTML(this.model.toJSON() ));
-    return this;
+    var template = '<p>{{ seatNumber }}</p>';
+    var templateFunction = Handlebars.compile(template);
+    var html = templateFunction({ seatNumber: this.model });
 
+
+    this.$el.html(html);
+
+    return this; // optional so I can do chaining
   },
 
   selectSeat: function() {
-    this.$el.parent().find('div').removeClass('highlight');
-    this.$el.addClass('highlight');
-    $('#showSeat').find('p').hide()(this.model.get('body')).fadeIn();
-  },
+    console.log('select seat: ', this.model)
+  }
+
 });
+
+
