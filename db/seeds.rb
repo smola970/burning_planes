5e44284ee26f7651b8441bc31069aa71acf3d9d2
Robert Will <mail.rwill@gmail.com>
2014-11-28 15:49:32 +1100

    seeding n starting to create reservations

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,7 @@ User.destroy_all
 admin = User.create(username: "admin", password: "password", password_confirmation: "password", admin: true)
 
 user = User.create(username: "customer", password: "password", password_confirmation: "password", admin: false)
+
+flight = Flight.create(flight_number: "QF 93", origin: "MEL", destination: "LAX", date: "19/11/2014", plane_id: 1)
+
+airplane = Airplane.create(name: "A380", rows: 10, columns: 4)
